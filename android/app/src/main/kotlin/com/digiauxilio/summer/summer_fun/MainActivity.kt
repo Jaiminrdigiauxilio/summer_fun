1f489bdf0998dfbbc54e2193552e71d78f0f7093
Jaiminrdigiauxilio <153737984+Jaiminrdigiauxilio@users.noreply.github.com>
2024-03-15 20:03:09 +0530

    initial project setup

---
+++ android/app/src/main/kotlin/com/digiauxilio/summer/summer_fun/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.digiauxilio.summer.summer_fun
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:summer_fun/screens/splashView.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    return MaterialApp(
+      title: 'Summer Fun',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
